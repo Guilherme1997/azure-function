f5fc10066417bfb618d413436eeb2be457edc77b
Guilherme1997 <guilhermego1997@gmail.com>
2021-11-24 22:19:50 -0300

    Primeiro commit

---
+++ NotificacaoAzuereFunction.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace NotificacaoAzuereFunction
+{
+
+    public static class NotificacaoAzuereFunction
+    {
+
+        [FunctionName("NotificacaoAzuereFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+   
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            HttpClient client = new HttpClient(clientHandler);
+
+            dynamic jsonObject = new {};
+
+            var content = new StringContent(jsonObject.ToString(), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("http://localhost:5084/notificacao", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return new OkObjectResult(responseString);
+        }
+    }
+}
